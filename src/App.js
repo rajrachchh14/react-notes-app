1a27ee849d3db1fad1ae69ee63ecd575096ad3a4
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-22 23:23:09 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
-
 import Header from './Header';
 import BodyPart from './BodyPart';
+import Display from './Display';
 
 export default function App() {
   return (
     <>
       <Header />
       <BodyPart />
+      <Display />
     </>
   );
 }
