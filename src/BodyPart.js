931b65db70072289b7fa5d6b6a2163a0b0e92366
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-22 23:13:23 +0530

    1 file modified

--- src/BodyPart.js
+++ src/BodyPart.js
@@ -5,6 +5,13 @@ export default function BodyPart() {
     input1: '',
     input2: '',
   });
+
+  let inputEvent = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    // ('ss');
+  };
   return (
     <>
       {/* hidden */}
@@ -16,33 +23,45 @@ export default function BodyPart() {
         </div>
       </div> */}
 
-      <main>
-        <div id="form-container">
-          <form id="form" autocomplete="off">
-            <input id="note-title" placeholder="Title" type="text" />
-            <input id="note-text" placeholder="Take a note..." type="text" />
-            <div id="form-buttons">
-              <button type="submit" id="submit-button">
-                Submit
-              </button>
-              <button type="button" id="form-close-button">
-                Close
-              </button>
-            </div>
-          </form>
-        </div>
-        <div id="notes">
-          <div class="note">
-            <div class="note-title">a</div>
-            <div class="note-text">b</div>
-            <div class="toolbar-container">
-              <div class="toolbar">
-                <i class="toolbar-delete">Delete</i>
-              </div>
+      <div id="form-container">
+        <form id="form" autocomplete="off">
+          <input
+            id="note-title"
+            name="input1"
+            value={formInput.input1}
+            placeholder="Title"
+            type="text"
+            onChange={inputEvent}
+          />
+          <input
+            id="note-text"
+            name="input2"
+            value={formInput.input1}
+            placeholder="Take a note..."
+            type="text"
+            onChange={inputEvent}
+          />
+          <div id="form-buttons">
+            <button type="submit" id="submit-button">
+              Submit
+            </button>
+            <button type="button" id="form-close-button">
+              Close
+            </button>
+          </div>
+        </form>
+      </div>
+      <div id="notes">
+        <div class="note">
+          <div class="note-title">a</div>
+          <div class="note-text">b</div>
+          <div class="toolbar-container">
+            <div class="toolbar">
+              <i class="toolbar-delete">Delete</i>
             </div>
           </div>
         </div>
-      </main>
+      </div>
     </>
   );
 }
