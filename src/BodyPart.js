ad9e4ff777543501675dd80d3f4503f44bc815ca
rajrachchh14 <rajrachchh14@gmail.com>
2021-09-22 22:48:29 +0530

    2 files modified

--- src/BodyPart.js
+++ src/BodyPart.js
@@ -4,14 +4,14 @@ export default function BodyPart() {
   return (
     <>
       {/* hidden */}
-      <div class="modal">
+      {/* <div class="modal">
         <div class="modal-content">
           <input class="modal-title" placeholder="Title" type="text" />
           <input class="modal-text" placeholder="Take a note..." type="text" />
           <span class="modal-close-button">Close</span>
         </div>
-      </div>
-      {/* hidden */}
+      </div> */}
+
       <main>
         <div id="form-container">
           <form id="form" autocomplete="off">
@@ -28,19 +28,6 @@ export default function BodyPart() {
           </form>
         </div>
         <div id="notes"></div>
-        <div id="placeholder">
-          <img
-            id="placeholder-logo"
-            src="https://icon.now.sh/lightbulb_outline"
-          />
-        </div>
-
-        <div id="color-tooltip">
-          <div class="color-option" data-color="#fff" id="white"></div>
-          <div class="color-option" data-color="#d7aefb" id="purple"></div>
-          <div class="color-option" data-color="#fbbc04" id="orange"></div>
-          <div class="color-option" data-color="#a7ffeb" id="teal"></div>
-        </div>
       </main>
     </>
   );
